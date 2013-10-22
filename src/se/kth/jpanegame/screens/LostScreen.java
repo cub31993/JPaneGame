a70d70c0beb57db5cb7f442ca2651ffe3ca761b0
Fredrik Laason <laason.fredrik@gmail.com>
2013-10-22 10:02:20 +0200

    some menu

--- src/se/kth/jpanegame/screens/LostScreen.java
+++ src/se/kth/jpanegame/screens/LostScreen.java
@@ -1,5 +1,10 @@
 package se.kth.jpanegame.screens;
 
+import se.kth.jpanegame.Input;
+
+import java.awt.*;
+import java.awt.event.KeyEvent;
+
 /**
  * Created with IntelliJ IDEA.
  * User: Fredrik
@@ -8,4 +13,36 @@ package se.kth.jpanegame.screens;
  * To change this template use File | Settings | File Templates.
  */
 public class LostScreen extends Screen {
+
+    private Font titleFont;
+    private Font menuFont;
+
+    public LostScreen() {
+        titleFont = new Font("TimesRoman", Font.BOLD, 34);
+        menuFont = new Font("TimesRoman", Font.PLAIN, 20);
+    }
+
+    public void update(Input input) {
+        if (input.isKeyDown(KeyEvent.VK_S)) {
+            startGame();
+        }
+
+        if (input.isKeyDown(KeyEvent.VK_Q)) {
+            game.stop();
+        }
+    }
+
+    public void render(Graphics g) {
+
+        g.setColor(Color.WHITE);
+        g.setFont(titleFont);
+        g.drawString("2DPlatform", 100, 100);
+        g.setFont(menuFont);
+        g.drawString("S: Restart Game", 100, 150);
+        g.drawString("Q: Quit", 100, 200);
+    }
+
+    public void startGame() {
+        this.setScreen(new GameScreen());
+    }
 }
